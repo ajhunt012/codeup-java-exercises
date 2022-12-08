c48c24610375d84fe29b9fef1698628e0ce03e94
Austin Hunt <austin.hunt.012@gmail.com>
2022-12-08 14:24:47 -0600

    initial exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+
+    }
+        //main method shortcut
+        //main(tab)
+        //psvm(tab/enter)
+//        public static void main(String[] args) {
+//    }
+
+//    System.out.println shortcut
+//            sout
+
+}
