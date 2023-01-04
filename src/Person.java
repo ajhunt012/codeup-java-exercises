67c8e84d3f4d977590814357d7893e36c997f9b5
Austin Hunt <austin.hunt.012@gmail.com>
2023-01-04 13:39:32 -0600

    added exercises and lectures

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,12 @@
-public class Person {
-    protected String name;
-    private int age;
+class Person {
+    private String name;
+    public Person(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
 
 //    public String name;
 
@@ -20,31 +26,21 @@ public class Person {
 //    public Person(String name){
 //        this.name = name;
 //    }
-//
-//
+
 //    public String getName(){
-////TODO: return the person's name
-//        return this.name;
-//    }
 //
 //    public void setName(String name){
-////TODO: change the name field to the passed value
-//        this.name = name;
-//    }
 //    public void sayHello(){
-////TODO: print a message to the console using the person's name
-//        System.out.println("Hello" + this.name);
-//    }
 
     public static void main(String[] args){
 //        Person person1 = new Person("John");
 //        Person person2 = new Person("John");
 //        System.out.println(person1.getName().equals(person2.getName()));// true because John and John are the same
-//        System.out.println(person1 == person2); // false, b/c theyre two  different instances
+//        System.out.println(person1 == person2); // false, b/c they're two  different instances
 
 //        Person person1 = new Person("John");
 //        Person person2 = person1; // assigning the same instance
-//        person1.setName("Wilmarie");
+//        person1.setName("WilMarie");
 //        System.out.println(person1 == person2);
 
 //        Person person1 = new Person("John");
