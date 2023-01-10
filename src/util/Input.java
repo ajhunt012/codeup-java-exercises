b304bccd0e882f97ba8d48c3d485fff4edff0124
Austin Hunt <austin.hunt.012@gmail.com>
2023-01-10 10:09:13 -0600

    reformatted getInt andgetDouble on Input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Input {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String getString(){
         return scanner.nextLine();
@@ -15,32 +15,77 @@ public class Input {
         return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("si");
     }
 
-    public static int getInt(int min, int max){
-        System.out.printf("Enter a number between %d and %d", min, max);
-        int userNumber = scanner.nextInt();
-        if(userNumber < min || userNumber > max){
-            getInt(min, max);
+    public static int getInt(int min, int max) {
+        while (true) {
+            System.out.printf("Enter a number between %d and %d: ", min, max);
+            String input = Input.getString();
+            try {
+                int userNumber = Integer.valueOf(input);
+                if (userNumber >= min && userNumber <= max) {
+                    return userNumber;
+                } else {
+                    System.out.println("Number/numbers was/were outside of specified range, Please enter a number within the specified range.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter an integer.");
+            }
         }
-        return userNumber;
-
     }
-
     public static int getInt(){
         return scanner.nextInt();
     }
 
-    public static double getDouble(double min, double max){
-        System.out.printf("Enter a number between %s and %s", min, max);
-        double userNumber = scanner.nextDouble();
-        if(Double.compare(userNumber, min) < 0 || Double.compare(userNumber, max) > 0 ){
-            getDouble(min, max);
+    public static double getDouble(double min, double max) {
+        while (true) {
+            System.out.printf("Enter a number between %s and %s: ", min, max);
+            String input = Input.getString();
+            try {
+                double userNumber = Double.valueOf(input);
+                if (Double.compare(userNumber, min) >= 0 && Double.compare(userNumber, max) <= 0) {
+                    return userNumber;
+                } else {
+                    System.out.println("Number/numbers was/were outside of specified range, Please enter a number within the specified range.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a double.");
+            }
         }
-        return userNumber;
     }
 
-    public static double getDouble(){
-        return scanner.nextDouble();
-
+    public static double getDouble() {
+        return getDouble(Double.MIN_VALUE, Double.MAX_VALUE);
     }
+
+    public static int getBinary(String prompt) {
+        System.out.print(prompt);
+        String input = Input.getString();
+        try {
+            return Integer.valueOf(input, 2);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input, please enter a binary number.");
+            return getBinary(prompt);
+        }
+    }
+
+    public static int getHex(String prompt) {
+        System.out.print(prompt);
+        String input = Input.getString();
+        try {
+            return Integer.valueOf(input, 16);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input, please enter a hexadecimal number.");
+            return getHex(prompt);
+        }
+    }
+
+//    call either of these two with the following code.
+//
+//    int binary = getBinary("Enter a binary number: ");
+//System.out.println("Your number is " + binary);
+//
+//    int hex = getHex("Enter a hexadecimal number: ");
+//System.out.println("Your number is " + hex);
+
+
 
 }
