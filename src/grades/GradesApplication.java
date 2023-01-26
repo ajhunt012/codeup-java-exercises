a4fcba9ead68d07103ad356e5a31b46b33117814
Austin Hunt <austin.hunt.012@gmail.com>
2023-01-26 11:12:56 -0600

    completed GradesApp

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -34,29 +34,29 @@ public class GradesApplication {
 
         HashMap<String, Student> students = new HashMap<>();
 
-        Student joshua = new Student("joshua");
-        joshua.addGrade(89);
-        joshua.addGrade(100);
-        joshua.addGrade(50);
-        students.put("joshua-m", joshua);
+        Student Josh = new Student("Josh");
+        Josh.addGrade(89);
+        Josh.addGrade(100);
+        Josh.addGrade(50);
+        students.put("Josh01", Josh);
 
-        Student roger = new Student("roger");
-        roger.addGrade(98);
-        roger.addGrade(72);
-        roger.addGrade(83);
-        students.put("rogerthedude", roger);
+        Student Jim = new Student("Jim");
+        Jim.addGrade(98);
+        Jim.addGrade(71);
+        Jim.addGrade(80);
+        students.put("Jim01", Jim);
 
-        Student doug = new Student("doug");
-        doug.addGrade(54);
-        doug.addGrade(105);
-        doug.addGrade(79);
-        students.put("douggie", doug);
+        Student Jack = new Student("Jack");
+        Jack.addGrade(53);
+        Jack.addGrade(100);
+        Jack.addGrade(69);
+        students.put("Jack01", Jack);
 
-        Student louise = new Student("louise");
-        louise.addGrade(100);
-        louise.addGrade(99);
-        louise.addGrade(97);
-        students.put("louise-apple", louise);
+        Student Jag = new Student("Jag");
+        Jag.addGrade(100);
+        Jag.addGrade(99);
+        Jag.addGrade(98);
+        students.put("Mr.Jag", Jag);
 
         studentInformation(students);
     }
