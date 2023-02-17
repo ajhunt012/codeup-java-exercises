e3987f094c7d3546cf6285c008bbb97d4a6259af
Austin Hunt <austin.hunt.012@gmail.com>
2023-02-17 12:34:11 -0600

    updated movies app

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,14 +1,40 @@
 package movies;
-
+import util.Input;
 import java.util.Scanner;
 
 public class MoviesApplication {
-    private static final Movie[] movies = MoviesArray.findAll();
 
-    public static void main(String[] args) {
+    public static void getMovieInfo(Movie movie){
+        System.out.println("Movie Name: " + movie.getName() + " | Movie Category: " + movie.getCategory());
+    }
+    public static void searchMovies(String genre) {
+        if (genre.equals("all")) {
+            for (Movie movie : MoviesArray.findAll()) {
+                getMovieInfo(movie);
+            }
+            goAgain();
+        } else {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals(genre)) {
+                    getMovieInfo(movie);
+                }
+            }
+            goAgain();
+        }
+    }
+
+    public static void goAgain() {
+        System.out.println("***********************\nSearch more movies? (y/n)");
         Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        if (answer.equals("y")) {
+            movieApplication();
+        } else {
+            System.out.println("Have a nice day.");
+        }
+    }
 
-        // Display options to the user
+    public static void movieApplication(){
         System.out.println("What would you like to do?");
         System.out.println("0 - exit");
         System.out.println("1 - view all movies");
@@ -16,10 +42,24 @@ public class MoviesApplication {
         System.out.println("3 - view movies in the drama category");
         System.out.println("4 - view movies in the horror category");
         System.out.println("5 - view movies in the scifi category");
-        System.out.print("Enter your choice: ");
+        System.out.println("Enter your choice: ");
+        int userInput = Input.getInt();
+        if (userInput == 1) {
+            searchMovies("all");
+        } else if (userInput == 2) {
+            searchMovies("animated");
+        } else if (userInput == 3) {
+            searchMovies("drama");
+        } else if (userInput == 4) {
+            searchMovies("horror");
+        } else if (userInput == 5) {
+            searchMovies("scifi");
+        } else {
+            System.out.println("Have a nice day.");
+        }
+    }
 
-        for (Movie movie : movies) {
-           System.out.println(movie.getName() + " - " + movie.getCategory());
-     }
-   }
+    public static void main(String[] args) {
+        movieApplication();
+    }
 }
